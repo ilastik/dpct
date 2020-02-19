d87c32e7d5dd5463ec13fbac7bc9d18d4b6be781
k-dominik <k-dominik@users.noreply.github.com>
2020-02-19 14:19:37 +0100

    fix boost build with boost >= 1.70

--- conda-recipe/build.sh
+++ conda-recipe/build.sh
@@ -16,6 +16,10 @@ cmake .. \
     -DWITH_LOG=OFF \
     -DCMAKE_CXX_LINK_FLAGS="${LDFLAGS}" \
     -DCMAKE_EXE_LINKER_FLAGS="${LDFLAGS}" \
+    -DBoost_INCLUDE_DIR=${PREFIX}/include \
+    -DBoost_LIBRARY_DIRS=${PREFIX}/lib \
+    -DBoost_PYTHON_LIBRARY=${PREFIX}/lib/libboost_python${CONDA_PY}${SHLIB_EXT} \
+    -DBoost_NO_BOOST_CMAKE=ON \
 
 make -j${CPU_COUNT}
 make install
