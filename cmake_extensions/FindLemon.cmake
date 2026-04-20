95cbe7b7c9d296e4d1902aeff0b9ecb04c36973f
k-dominik <k-dominik@users.noreply.github.com>
2026-04-20 13:19:40 +0200

    Consistent naming usage for Lemon
    Lemon

--- cmake_extensions/FindLemon.cmake
+++ cmake_extensions/FindLemon.cmake
@@ -1,21 +1,21 @@
 # This module finds an installed Lemon package.
 #
 # It sets the following variables:
-#  LEMON_FOUND              - Set to false, or undefined, if lemon isn't found.
-#  LEMON_INCLUDE_DIR        - Lemon include directory.
-#  LEMON_LIBRARIES          - Lemon library files
-FIND_PATH(LEMON_INCLUDE_DIR lemon/config.h PATHS /usr/include /usr/local/include ${CMAKE_INCLUDE_PATH} ${CMAKE_PREFIX_PATH}/include $ENV{LEMON_ROOT}/include ENV CPLUS_INCLUDE_PATH)
-FIND_LIBRARY(LEMON_LIBRARIES 
-  NAMES emon lemon 
-  PATHS $ENV{LEMON_ROOT}/src/impex $ENV{LEMON_ROOT}/lib ENV LD_LIBRARY_PATH ENV LIBRARY_PATH
+#  Lemon_FOUND              - Set to false, or undefined, if lemon isn't found.
+#  Lemon_INCLUDE_DIR        - Lemon include directory.
+#  Lemon_LIBRARIES          - Lemon library files
+FIND_PATH(Lemon_INCLUDE_DIR lemon/config.h PATHS /usr/include /usr/local/include ${CMAKE_INCLUDE_PATH} ${CMAKE_PREFIX_PATH}/include $ENV{Lemon_ROOT}/include ENV CPLUS_INCLUDE_PATH)
+FIND_LIBRARY(Lemon_LIBRARIES
+  NAMES emon lemon
+  PATHS $ENV{Lemon_ROOT}/src/impex $ENV{Lemon_ROOT}/lib ENV LD_LIBRARY_PATH ENV LIBRARY_PATH
 )
 
-GET_FILENAME_COMPONENT(LEMON_LIBRARY_PATH ${LEMON_LIBRARIES} PATH)
-SET( LEMON_LIBRARY_DIR ${LEMON_LIBRARY_PATH} CACHE PATH "Path to lemon library.")
+GET_FILENAME_COMPONENT(Lemon_LIBRARY_PATH ${Lemon_LIBRARIES} PATH)
+SET( Lemon_LIBRARY_DIR ${Lemon_LIBRARY_PATH} CACHE PATH "Path to lemon library.")
 
-# handle the QUIETLY and REQUIRED arguments and set LEMON_FOUND to TRUE if 
+# handle the QUIETLY and REQUIRED arguments and set Lemon_FOUND to TRUE if
 # all listed variables are TRUE
 INCLUDE(FindPackageHandleStandardArgs)
-FIND_PACKAGE_HANDLE_STANDARD_ARGS(LEMON DEFAULT_MSG LEMON_LIBRARIES LEMON_INCLUDE_DIR)
+FIND_PACKAGE_HANDLE_STANDARD_ARGS(Lemon DEFAULT_MSG Lemon_LIBRARIES Lemon_INCLUDE_DIR)
 
-MARK_AS_ADVANCED( LEMON_INCLUDE_DIR LEMON_LIBRARIES LEMON_LIBRARY_DIR )
+MARK_AS_ADVANCED( Lemon_INCLUDE_DIR Lemon_LIBRARIES Lemon_LIBRARY_DIR )
