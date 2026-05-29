7d8068b79fa1dd575e4910cb2bff85590b62d92a
k-dominik <k-dominik@users.noreply.github.com>
2026-05-29 11:40:48 +0200

    Add some for win

--- conda-recipe/build.bat
+++ conda-recipe/build.bat
@@ -29,5 +29,10 @@ if errorlevel 1 exit 1
 nmake all
 if errorlevel 1 exit 1
 
-nmake install
-if errorlevel 1 exit 1
+REM This is for the conda recipe really - the tests don't define install targets
+REM so if these are run in isolation, we want to skip.
+
+if /I "%WITH_TESTS%"=="OFF" (
+    nmake install
+    if errorlevel 1 exit 1
+)
